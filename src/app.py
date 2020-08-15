ae89e0ef73df6cc643472b7b222c74a4e00a4107
naf <naf@auri.ga>
2020-08-15 18:26:15 +0900

    envを読むように

--- src/app.py
+++ src/app.py
@@ -1,10 +1,13 @@
+import os
 from flask import Flask, request
 app = Flask(__name__)
+
+dbConfigs = { "HOST_IP": os.environ.get("HOST_IP"), "HOST_PORT": os.environ.get("HOST_PORT"), "DB_PORT": os.environ.get("DB_PORT"), "DB_USER": os.environ.get("DB_USER"), "DB_PASS": os.environ.get("DB_PASS") }
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
-@app.route('/sample', methods=['POST'])
+@app.route('/sample', methods=['GET'])
 def sample_post():
-    return "param1:{}".format(request.form['param1'])
+    return "param1:{}".format(dbConfigs)
